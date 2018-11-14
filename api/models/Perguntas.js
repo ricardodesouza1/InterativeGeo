b785e79856ba33b9dfe8e116089ab1ea79344cc6
Ricardo de Souza <ricardo270296souza@gmail.com>
2018-11-13 22:41:35 -0200

    crição perguntas

---
+++ api/models/Perguntas.js
@@ -0,0 +1,48 @@
+/**
+ * Pessoa.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    perguntas: {
+      type:"string", 
+      required:true,
+      minLength: 2
+    },
+    alternativaA: {
+	
+
+    },
+    alternativaB: {
+
+    },
+    alternativaC: {
+
+    },
+    alternativaD: {
+
+    },
+    alternativaCoreta: {
+
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
